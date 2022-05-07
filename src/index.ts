5932bc6e257b43b65da994c265909979885663f7
thestriver <wannabeamumin@gmail.com>
2022-05-07 09:40:00 +0100

    const

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,28 @@ interface Streak {
     currentCount: number
     startDate: string
     lastLoginDate: string
-  }
-  const KEY = 'streak'
+}
+const KEY = 'streak'
 
-  export function streakCounter(storage: Storage, date: Date): Streak {
-     const streakInLocalStorage = storage.getItem(KEY);
-     if (streakInLocalStorage) {
-       try {
-         const streak = JSON.parse(streakInLocalStorage || "");
-         return streak;
-       } catch (error) {
-         console.error("Failed to parse streak from localStorage");
-       }
-     }
-    
-     const streak = {
-       currentCount: 1,
-       startDate: formattedDate(date),
-       lastLoginDate: formattedDate(date),
-     };
-    
-     // store in localStorage
-     storage.setItem(KEY, JSON.stringify(streak));
-    
-     return streak;
+export const streakCounter = (storage: Storage, date: Date): Streak => {
+    const streakInLocalStorage = storage.getItem(KEY);
+    if (streakInLocalStorage) {
+      try {
+        const streak = JSON.parse(streakInLocalStorage || "");
+        return streak;
+      } catch (error) {
+        console.error("Failed to parse streak from localStorage");
+      }
     }
+  
+    const streak = {
+      currentCount: 1,
+      startDate: formattedDate(date),
+      lastLoginDate: formattedDate(date),
+    };
+  
+    // store in localStorage
+    storage.setItem(KEY, JSON.stringify(streak));
+  
+    return streak;
+  }
